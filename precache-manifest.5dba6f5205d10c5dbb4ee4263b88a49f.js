e643382b6625dd0161c51733f179fabf0a8c0473
amirsayeed <amirsayeed090@gmail.com>
2021-02-20 15:21:32 +0600

    Updates

---
+++ precache-manifest.5dba6f5205d10c5dbb4ee4263b88a49f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe947d6be8e11106d5d291888dd4aa9c",
+    "url": "/Photo-gallery/index.html"
+  },
+  {
+    "revision": "920c34a91d862366a8f0",
+    "url": "/Photo-gallery/static/css/main.bcc1763e.chunk.css"
+  },
+  {
+    "revision": "15cdc8ca9950de6205ca",
+    "url": "/Photo-gallery/static/js/2.5168fe5a.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/Photo-gallery/static/js/2.5168fe5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "920c34a91d862366a8f0",
+    "url": "/Photo-gallery/static/js/main.98632cf1.chunk.js"
+  },
+  {
+    "revision": "efd9f12f9d67223675db",
+    "url": "/Photo-gallery/static/js/runtime-main.cd178223.js"
+  }
+]);
